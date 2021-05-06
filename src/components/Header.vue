fac155749c39d3f7394abffcf760376cf313ae2c
Jeremie Cook <hello@jeremiecook.com>
2021-05-06 12:07:42 +0200

    fix menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -37,17 +37,17 @@
               >
             </li>
             <li class="fr-nav__item">
-              <g-link class="fr-nav__link" to="/events" target="_self"
+              <g-link class="fr-nav__link" to="/events/" target="_self"
                 >Évènements</g-link
               >
             </li>
             <li class="fr-nav__item">
-              <g-link class="fr-nav__link" to="/guides" target="_self"
+              <g-link class="fr-nav__link" to="/guides/" target="_self"
                 >Guides</g-link
               >
             </li>
             <li class="fr-nav__item">
-              <g-link class="fr-nav__link" to="/stats" target="_self"
+              <g-link class="fr-nav__link" to="/stats/" target="_self"
                 >Statistiques</g-link
               >
             </li>
